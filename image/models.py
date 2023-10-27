09c4a42e2beb10a0945d38ec4d44cf0f741778bb
jod431047 <jod431047@gmail.com>
2023-10-27 19:04:25 +0200

    add models & admin in project

--- image/models.py
+++ image/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+IMAGE_COLOUR =(
+    ('red','red'),
+    ('blue','blue'),
+    ('white','white'),
+    ('black','black'),
+    
+)
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=1000)
+    colour = models.CharField(max_length=20,choices=IMAGE_COLOUR,default='red')
+    create_date = models.DateTimeField(_('create date'),default=timezone.now)
+    
+    
+    def __str__(self):
+        return self.name
