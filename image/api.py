c7c2c1cb528d2fdbd07d4663596c11cc9a0097fc
jod431047 <jod431047@gmail.com>
2023-10-27 19:52:20 +0200

    add Serializers

--- image/api.py
+++ image/api.py
@@ -1,14 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
+from .serializers import ImageSerializers
 from .models import Image
 
 
 class ImageListAPI(generics.ListAPIView):
     queryset = Image.objects.all()
+    serializer_class = ImageSerializers
   
     
     
 class ImageDetailAPI(generics.RetrieveAPIView):
     queryset = Image.objects.all()
-   
+    serializer_class = ImageSerializers
